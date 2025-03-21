37aa0ed2e41c6f6e47f36ca2b9fb5a8a58609a70
KawadeSaurabh <kawade.saurabh1996@gmail.com>
2025-03-21 17:14:41 +0530

    Fourth commit

---
+++ Student.java
@@ -0,0 +1,26 @@
+
+public class Student {
+    private String name;
+    private int age;
+    private String course;
+
+    // Constructor
+    public Student(String name, int age, String course) {
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    // Method to display student details
+    public void displayStudentInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        Student student1 = new Student("Saurabh Kawade", 25, "Java Development");
+        student1.displayStudentInfo();
+    }
+}
