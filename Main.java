20112a44ffa2582b9b07ea1826e8fb8fcb92771d
KawadeSaurabh <kawade.saurabh1996@gmail.com>
2025-03-21 17:09:15 +0530

    Second commit

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello Jenkins+Java");
+        System.out.println("Hello Java");
+        System.out.println("Hello Jenkins");
     }
 }
