998c1f6791f859cbef637073ac5d851d13d2631e
KawadeSaurabh <kawade.saurabh1996@gmail.com>
2025-03-22 22:34:12 +0530

    Eighth Hit

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello Jenkins+Java");
-        System.out.println("Hello Java");
-        System.out.println("Hello Jenkins");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
 
-         System.out.println("Hello Jenkins+Java");
-        System.out.println("Hello Java");
-        System.out.println("Hello Jenkins");
     }
 }
